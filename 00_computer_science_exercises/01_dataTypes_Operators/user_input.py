193d9ffffd15321801e0ea6726bb77edc45a69ef
Andpatt616 <s12421244@students.duvalschools.org>
2024-09-06 11:34:57 -0400

    update 2

--- 00_computer_science_exercises/01_dataTypes_Operators/user_input.py
+++ 00_computer_science_exercises/01_dataTypes_Operators/user_input.py
@@ -1,3 +1,23 @@
+# User Input Practice, Andrew Patton, v0v0
+
+# input() is the built-in to get information from the KEYBOARD
+# BASIC EXAMPLE
+# variableName = input("please type a variable name and press enter.\n")
+# print(variableName)
+
+# input() saves ALL INPUT TO STRING DATA TYPES DEFAULT!!!
+# input() saves ALL INPUT TO STRING DATA TYPES DEFAULT!!!
+# input() saves ALL INPUT TO STRING DATA TYPES DEFAULT!!!
+# input() saves ALL INPUT TO STRING DATA TYPES DEFAULT!!!
+
+# INCORRECT, CAUSES A CONCENTRACTION ERROR.
+# myNumber = input("Please type an INTEGER number and press enter.\n")
+# print(myNumber = 5)
+
+# CORRECT -- USE a wrpper
+myNumber = int(input("Please type an INTEGER nuber and press enter.\n"))
+print(myNumber + 5)
+
 # Wrapper Functions
 # int() will convert the data to an INTEGER if possible.
 newNumber = input("Please type a value and press enter.\n")
@@ -10,3 +30,8 @@ newNumber = input("Please type a value and press enter.\n")
 # print(int(newNumber)) <-- cannot convert FLOAT to INTEGER.
 print(float(newNumber)) # can convert STRING to FLOAT.
 print(str(newNumber)) # can convert FLOAT to STRING.
+
+# str() will convert the data to a STRING if possible.
+newNumber = 5
+print(newNumber + newNumber) # Should print 10
+print(str(newNumber + newNumber)) # Should print 55
