2f4998646d099db3ab7f511e17457421d5d4034f
Andpatt616 <s12421244@students.duvalschools.org>
2024-09-18 12:50:06 -0400

    DA RULES FOR RPS

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
@@ -7,7 +7,6 @@ import random
 playerScore = 0
 playerName = "Test Player"
 playerChoice = None
-cpuScore = 0
 
 # DATA STRUCTURES -- CPU
 cpuScore = 0
@@ -18,13 +17,27 @@ playerName = input ("Please type your name and press enter.\n")
 print("Hello {playerName}!\n")
 isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
 
-#.lower() can turn ALL 
+# .lower() can turn ALL input into lowercase.
+# .upper() can turn ALL input into uppercase.
+ 
 if isCorrect == "yes":
     print(f"Ok {playerName}, lets's play Rock, Paper, Scissors!\n")
 else:
     playerName = input ("Please type your name and press enter.\n")
 
+# THE RULES using MULTI-LINE STRING
+print(f"""
+Welcome, {playerName} to the Rock, Paper, Scissors Robot!
+It's Time To Play Rock, Paper, Scissors!
+      
+You will play against a CPU and the first one to reach 5 points wins/
+You will select either Rock, Paper, or Scissors.
+The CPU will also pick Rock, Paper, or Scissors but a random.
 
+-- ROCK BEATS SCISSORS
+-- SCISSORS BEATS PAPER
+-- PAPER BEATS ROCK
+""")
 
 
 
