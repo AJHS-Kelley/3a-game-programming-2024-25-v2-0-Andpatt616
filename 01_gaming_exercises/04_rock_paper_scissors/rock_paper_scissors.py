dc87b620c92d06a1065e6d2cb2eeba988c2eb1b5
Andpatt616 <s12421244@students.duvalschools.org>
2024-09-18 12:05:29 -0400

    lower and upper

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
@@ -1,4 +1,4 @@
-# Rock, Paper, Scissors by Andrew Patton, v0.0
+# Rock, Paper, Scissors by Andrew Patton, v0.1
 
 # MODULE IMPORTS
 import random
@@ -9,7 +9,23 @@ playerName = "Test Player"
 playerChoice = None
 cpuScore = 0
 
-
 # DATA STRUCTURES -- CPU
 cpuScore = 0
-cpuChoice = None 
+cpuChoice = None
+
+# PLAYER NAME INPUT
+playerName = input ("Please type your name and press enter.\n")
+print("Hello {playerName}!\n")
+isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
+
+#.lower() can turn ALL 
+if isCorrect == "yes":
+    print(f"Ok {playerName}, lets's play Rock, Paper, Scissors!\n")
+else:
+    playerName = input ("Please type your name and press enter.\n")
+
+
+
+
+
+
