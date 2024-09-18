00d736d61aa10a3a8ecbae1926f407cbf0c8657f
Andpatt616 <s12421244@students.duvalschools.org>
2024-09-18 13:17:44 -0400

    Main loop of RPS.py

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
@@ -1,4 +1,4 @@
-# Rock, Paper, Scissors by Andrew Patton, v0.1
+# Rock, Paper, Scissors by Andrew Patton, v0.2
 
 # MODULE IMPORTS
 import random
@@ -14,7 +14,7 @@ cpuChoice = None
 
 # PLAYER NAME INPUT
 playerName = input ("Please type your name and press enter.\n")
-print("Hello {playerName}!\n")
+print(f"Hello {playerName}!\n")
 isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
 
 # .lower() can turn ALL input into lowercase.
@@ -39,6 +39,29 @@ The CPU will also pick Rock, Paper, or Scissors but a random.
 -- PAPER BEATS ROCK
 """)
 
+# MULTI-LINE STRINGS CAN BE USED AS BIG COMMENTS
+"""
+Anything in between the sets of double-quotes is just ignore.
+If you need to write latge comments, it's easier to use multi-line strings than
+putting an # in front of 15 different lines.
+"""
+
+# MAIN GAME LOOP
+while playerScore < 5 and cpuScore < 5:
+    print(f"{playerName} you have {playerScore} pointa.\nThe CPU has {cpuScore} points.\n")
+    playerChoice = input("Please enter rock, paper, or scissors and press enter\n").lower()
+    if playerChoice != "rock" or playerChoice != "paper" or playerChoice != "scissors":
+        playerChoice = input("Please enter rock, paper, or scissors and press enter\n").lower()
+        if playerChoice != "rock" or playerChoice != "paper" or playerChoice != "scissors":
+            print("You are not following directions. Please try again.\n")
+            exit()
+        print(f"You have chosen {playerChoice}.\n")
+    else:
+        print(f"You have chosen {playerChoice}.\n")
 
 
 
+    # let cpu select choice at random.
+    # compare player choice to cpu choice
+    # print the results to the screen
+    # award point to winner and output results.
