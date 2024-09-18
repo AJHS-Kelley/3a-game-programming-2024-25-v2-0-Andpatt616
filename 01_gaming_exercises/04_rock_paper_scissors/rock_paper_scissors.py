2fecfbf59e276cb8a757734917201482f7f6df76
Andpatt616 <s12421244@students.duvalschools.org>
2024-09-18 11:50:36 -0400

    RPS Project

---
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,15 @@
+# Rock, Paper, Scissors by Andrew Patton, v0.0
+
+# MODULE IMPORTS
+import random
+
+# DATA STRUCTURES -- PLAYER
+playerScore = 0
+playerName = "Test Player"
+playerChoice = None
+cpuScore = 0
+
+
+# DATA STRUCTURES -- CPU
+cpuScore = 0
+cpuChoice = None 
