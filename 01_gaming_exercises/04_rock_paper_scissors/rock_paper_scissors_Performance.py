d98f2246055a34be627ce15d09632355c579ff82
Andpatt616 <s12421244@students.duvalschools.org>
2024-09-30 11:40:07 -0400

    RPS.py Performance

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors_Performance.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors_Performance.py
@@ -1,63 +1,19 @@
 # Rock, Paper, Scissors by Andrew Patton, v0.3
 
 # MODULE IMPORTS
-import random
+import random, time 
 
 # DATA STRUCTURES -- PLAYER
 playerScore = 0
-playerName = "Test Player"
 playerChoice = None
+numDraws = 0
 
 # DATA STRUCTURES -- CPU
 cpuScore = 0
 cpuChoice = None
 
-# PLAYER NAME INPUT
-playerName = input ("Please type your name and press enter.\n")
-print(f"Hello {playerName}!\n")
-isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
-
-# .lower() can turn ALL input into lowercase.
-# .upper() can turn ALL input into uppercase.
- 
-if isCorrect == "yes":
-    print(f"Ok {playerName}, lets's play Rock, Paper, Scissors!\n")
-else:
-    playerName = input ("Please type your name and press enter.\n")
-
-# THE RULES using MULTI-LINE STRING
-print(f"""
-Welcome, {playerName} to the Rock, Paper, Scissors Robot!
-It's Time To Play Rock, Paper, Scissors!
-      
-You will play against a CPU and the first one to reach 5 points wins.
-You will select either Rock, Paper, or Scissors.
-The CPU will also pick Rock, Paper, or Scissors but a random.
-
--- ROCK BEATS SCISSORS
--- SCISSORS BEATS PAPER
--- PAPER BEATS ROCK
-""")
-
-# MULTI-LINE STRINGS CAN BE USED AS BIG COMMENTS
-"""
-Anything in between the sets of double-quotes is just ignore.
-If you need to write latge comments, it's easier to use multi-line strings than
-putting an # in front of 15 different lines.
-"""
-
 # MAIN GAME LOOP
 while playerScore < 5 and cpuScore < 5:
-    print(f"{playerName} you have {playerScore} points.\nThe CPU has {cpuScore} points.\n")
-    playerChoice = input("Please enter rock, paper, or scissors and press enter\n").lower()
-    if playerChoice != "rock" and playerChoice != "paper" and playerChoice != "scissors":
-        playerChoice = input("Please enter rock, paper, or scissors and press enter\n").lower()
-        if playerChoice != "rock" and playerChoice != "paper" and playerChoice != "scissors":
-            print("You are not following directions. Please try again.\n")
-            exit()
-        print(f"You have chosen {playerChoice}.\n")
-    else:
-        print(f"You have chosen {playerChoice}.\n")
     
     # let cpu select choice at random.
     cpuChoice = random.randint(0, 2) # randomly select 0, 1, 2.
@@ -70,7 +26,19 @@ while playerScore < 5 and cpuScore < 5:
     else:
         print("Unable to determine CPU choice.\nPlease restart.\n")
         exit()
-    print(f"CPU choice: {cpuChoice}")
+    #print(f"CPU choice: {cpuChoice}")
+        
+    # let PLAYER select choice at random.
+    playerChoice = random.randint(0, 2) # randomly select 0, 1, 2.
+    if playerChoice == 0:
+        playerChoice = "rock"
+    elif playerChoice == 1:
+        playerChoice = "paper"
+    elif playerChoice == 2:
+        playerChoice = "scissors"
+    else:
+        print("Unable to determine CPU choice.\nPlease restart.\n")
+        exit()
 
     # compare player choice to cpu choice
     if playerChoice == "rock" and cpuChoice == "paper":
