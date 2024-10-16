889c2f1279e44373db22d81ded3dd6ec2d2a0e8f
Andpatt616 <s12421244@students.duvalschools.org>
2024-10-16 12:02:10 -0400

    defplayerName

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors3.0.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors3.0.py
@@ -13,17 +13,21 @@ cpuScore = 0
 cpuChoice = None
 
 # PLAYER NAME INPUT
-playerName = input ("Please type your name and press enter.\n")
-print(f"Hello {playerName}!\n")
-isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
-
-# .lower() can turn ALL input into lowercase.
-# .upper() can turn ALL input into uppercase.
- 
-if isCorrect == "yes":
-    print(f"Ok {playerName}, lets's play Rock, Paper, Scissors!\n")
-else:
+def playerName(): # Function Signature -- name of function, (anrguments if any)
     playerName = input ("Please type your name and press enter.\n")
+    print(f"Hello {playerName}!\n")
+    isCorrect = input("Is that correct? Type yes or no and press enter.\n").lower()
+
+    # .lower() can turn ALL input into lowercase.
+    # .upper() can turn ALL input into uppercase.
+ 
+    if isCorrect == "yes":
+        print(f"Ok {playerName}, lets's play Rock, Paper, Scissors!\n")
+    else:
+        playerName = input ("Please type your name and press enter.\n")
+
+# CALLING THE FUNCTION
+playerName()
 
 # THE RULES using MULTI-LINE STRING
 print(f"""
