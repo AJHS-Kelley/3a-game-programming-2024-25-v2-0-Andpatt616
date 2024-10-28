a430f54adcaa5870947b0f942add81a68e039b11
Andpatt616 <s12421244@students.duvalschools.org>
2024-10-28 12:05:05 -0400

    helloworldmulti

---
+++ 00_computer_science_exercises/05_functions/functionsPractice.py
@@ -0,0 +1,20 @@
+# Functions Practice, Andrew Patton, v0.0
+
+import random
+
+def helloWorldMulti(): # FUNCTION SIGNATURE
+    """This function will output Hello, World! in a language the user choose.""" # DOCSTRING /
+    # print a list of languages to the screen, at least three but no more than five.
+    print("""
+          
+          
+          
+          
+          
+          
+          """)
+    # allow the user to select (input) a choice for the language.
+    language = input("What language do you want?\n  Please type the language you want.\n")
+
+    # print "Hello, World!" to the screen in that language.
+    
